a10ae8939db126d0087745e1238d0d59a6d4e86e
Ana-HHH <97644103+Ana-HHH@users.noreply.github.com>
2022-10-23 17:23:24 -0500

    initial commit con plantilla y vistas

---
+++ resources/views/template.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Plantilla</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+</head>
+{{-- Tabs --}}
+<body>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Formulario</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tabla</a>
+        </li>
+    </ul>
+
+
+</body>
+</html>
